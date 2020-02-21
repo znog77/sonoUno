d230c9c9b7931faf294b895e6c2aab8461afcbc4
Gonzalo A. de la Vega <gadelavega@gmail.com>
2020-02-21 20:10:38 -0300

    Remove junk code from setup.py
    In an attempt to improve installation compatibility we tried to install
    wxPython from a non-PyPI source on Linux systems. This is not allowed
    by pip.

--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from setuptools import setup, find_packages
 import platform
 
-system_type = platform.uname()[0]
-if system_type == 'Linux':
-    dist_name = platform.dist()[0].lower()
-    dist_ver = platform.dist()[1].split('.')[0]  
-
-    minor=int(platform.python_version()[2])
-
-    if minor < 7:
-        cp = str(30+minor)
-    else:
-        cp = '37'
-
-    if dist_name == 'ubuntu':
-        if dist_ver == '14':
-            dist =  'ubuntu-14.04'
-        if dist_ver == '16':
-            dist = 'ubuntu-16.04'
-        if dist_ver == '18' or dist_ver == '19':
-           dist = 'ubuntu-18.04' 
-    else:
-        dist = '-'.join([dist_name, dist_ver])
-
-    wxpython_url = 'wxPython @ https://extras.wxpython.org/wxPython4/extras/linux/gtk3/'+dist+'/wxPython-4.0.7.post2-cp'+cp+'-cp'+cp+'m-linux_x86_64.whl'
-
-else:
-    wxpython_url = 'wxpython'
+#system_type = platform.uname()[0]
+#if system_type == 'Linux':
+#    dist_name = platform.dist()[0].lower()
+#    dist_ver = platform.dist()[1].split('.')[0]  
+#
+#    minor=int(platform.python_version()[2])
+#
+#    if minor < 7:
+#        cp = str(30+minor)
+#    else:
+#        cp = '37'
+#
+#    if dist_name == 'ubuntu':
+#        if dist_ver == '14':
+#            dist =  'ubuntu-14.04'
+#        if dist_ver == '16':
+#            dist = 'ubuntu-16.04'
+#        if dist_ver == '18' or dist_ver == '19':
+#           dist = 'ubuntu-18.04' 
+#    else:
+#        dist = '-'.join([dist_name, dist_ver])
+#
+#    wxpython_url = 'wxPython @ https://extras.wxpython.org/wxPython4/extras/linux/gtk3/'+dist+'/wxPython-4.0.7.post2-cp'+cp+'-cp'+cp+'m-linux_x86_64.whl'
+#
+#else:
+#    wxpython_url = 'wxpython'
 
 #Available wxPython repos:
 #centos-7
@@ -63,6 +63,6 @@ setup(name='sonoUno',
         'pytz',
         'six',
         'oct2py',
-        wxpython_url,
+        'wxpython',
       ],
       zip_safe=True)
